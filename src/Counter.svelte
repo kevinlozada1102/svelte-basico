c268d35426410e47d4b89b1937c46e6db6cb188c
kevinlozada1102 <kevinlozada1102@gmail.com>
2020-06-07 14:53:19 -0500

    componentes state

---
+++ src/Counter.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let name;
+    export let contadorInicial = 0;
+    let contador = contadorInicial;
+
+	function aumentarContador() {
+		contador++;
+	}
+</script>
+
+<main>
+	<button on:click={aumentarContador}>Contador</button>
+	<p>Contador click : {contador}</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
